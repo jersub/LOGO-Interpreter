883b369d3049759bde2a92f735a895cca54203d2
(no author) <(no author)@1e871849-ca45-4617-b4d0-c39c463a1ac7>
2010-04-15 22:30:33 +0000

    Classe Traceur documentée.
    git-svn-id: http://ladystorm.bigmadarea.net/svn/nf11@6 1e871849-ca45-4617-b4d0-c39c463a1ac7

--- src/logogui/Traceur.java
+++ src/logogui/Traceur.java
@@ -1,83 +1,109 @@
-/*
- * Created on 12 sept. 2008
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package logogui;
+
 import java.awt.Graphics2D;
 import java.awt.Graphics;
 
 /**
- * @author Claude Moulin
+ * Traceur dessinant les instructions LOGO interprétées.
  *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * @author Romain Fauvet
+ * @author Jérémy Subtil
  */
 public class Traceur {
-	private static Traceur instance;
-	private Graphics2D g2d;
-	private double initx = 300, inity = 300;   // position initiale
-	private double posx = initx, posy = inity; // position courante
-	private int angle = 90;
-	private double teta;
-	
-	public Traceur() {
-		setTeta();
-	}
 
-	/**
-	 * @return Returns the instance.
-	 */
-	public static Traceur getInstance() {
-		if (instance == null)
-			instance = new Traceur();
-		return instance;
-	}
+    private static Traceur instance;
+    private Graphics2D g2d;
+    private double initx = 300, inity = 300;   // position initiale
+    private double posx = initx, posy = inity; // position courante
+    private int angle = 90;
+    private double teta;
 
-	public void setGraphics(Graphics g) {
-		g2d = (Graphics2D) g;	
-	}
-	
-	private int toInt(double a) {
-		return (int) Math.round(a);
-	}
-	
-	public void avance(double r) {
-		double a = posx + r * Math.cos(teta) ;
-		double b = posy - r * Math.sin(teta) ;
-		int x1 = toInt(posx);
-		int y1 = toInt(posy);
-		int x2 = toInt(a);
-		int y2 = toInt(b);
-		g2d.drawLine(x1,y1,x2,y2);
-		posx = a;
-		posy = b;
-	}
-	
-	public void recule(double r) {
-		double a = posx - r * Math.cos(teta) ;
-		double b = posy + r * Math.sin(teta) ;
-		int x1 = toInt(posx);
-		int y1 = toInt(posy);
-		int x2 = toInt(a);
-		int y2 = toInt(b);
-		g2d.drawLine(x1,y1,x2,y2);
-		posx = a;
-		posy = b;
-	}
-	
-	public void td(double r) {
-		angle = (angle - toInt(r)) % 360;
-		setTeta();
-	}
-	
-	public void tg(double r) {
-		angle = (angle + toInt(r)) % 360;
-		setTeta();
-	}
-	
-	private void setTeta() {
-		teta = Math.toRadians(angle);
-	}
+    /**
+     * Constructeur.
+     */
+    public Traceur() {
+        setTeta();
+    }
+
+    /**
+     *Retourne l'instance du traceur.
+     *
+     * @return l'instance singleton
+     */
+    public static Traceur getInstance() {
+        if (instance == null) {
+            instance = new Traceur();
+        }
+        return instance;
+    }
+
+    /**
+     * Définit le support de dessin du traceur.
+     *
+     * @param g  le support de dessin
+     */
+    public void setGraphics(Graphics g) {
+        g2d = (Graphics2D) g;
+    }
+
+    /**
+     * Trace en avant.
+     *
+     * @param r  la longueur à tracer
+     */
+    public void avance(double r) {
+        double a = posx + r * Math.cos(teta);
+        double b = posy - r * Math.sin(teta);
+        int x1 = toInt(posx);
+        int y1 = toInt(posy);
+        int x2 = toInt(a);
+        int y2 = toInt(b);
+        g2d.drawLine(x1, y1, x2, y2);
+        posx = a;
+        posy = b;
+    }
+
+    /**
+     * Trace en arrière.
+     *
+     * @param r  la longueur à tracer
+     */
+    public void recule(double r) {
+        double a = posx - r * Math.cos(teta);
+        double b = posy + r * Math.sin(teta);
+        int x1 = toInt(posx);
+        int y1 = toInt(posy);
+        int x2 = toInt(a);
+        int y2 = toInt(b);
+        g2d.drawLine(x1, y1, x2, y2);
+        posx = a;
+        posy = b;
+    }
+
+    public void td(double r) {
+        angle = (angle - toInt(r)) % 360;
+        setTeta();
+    }
+
+    public void tg(double r) {
+        angle = (angle + toInt(r)) % 360;
+        setTeta();
+    }
+
+    /**
+     * Définit l'angle têta en radians à partir de l'angle définit en degrés.
+     */
+    private void setTeta() {
+        teta = Math.toRadians(angle);
+    }
+
+    /**
+     * Transtype le nombre double donné en int.
+     *
+     * @param a  la valeur à transtyper
+     * @return   la valeur arrondie en int
+     */
+    private int toInt(double a) {
+        return (int) Math.round(a);
+    }
 }
